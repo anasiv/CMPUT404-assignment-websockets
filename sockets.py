d623b9d6bf50504a40af63473d5bb1aac49a24bc
Nasiv <anasiv350@gmail.com>
2022-12-02 17:20:23 -0700

    gevent errors

--- sockets.py
+++ sockets.py
@@ -135,6 +135,8 @@ def subscribe_socket(ws):
         clients.remove(client)
         gevent.kill(g)
 
+    return None
+
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
